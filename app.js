e086e7b4ba7b4220f9362750bfcf1f1b264cea0e
ojslick <seun407@gmail.com>
2020-08-01 15:29:08 +0100

    fixed cors error 5

--- app.js
+++ app.js
@@ -10,18 +10,17 @@ const HttpError = require('./models/http-error');
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://echtezalm.herokuapp.com'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-  next();
-});
+app.use(cors());
+
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+//   next();
+// });
 
 app.use('/api/users', userRoutes);
 
